b6ac79c2124e41a058b1d09dfe2df563920fa566
aismai <abai.bakytbek@gmail.com>
2024-01-27 15:48:56 +0300

    initial commit

---
+++ char.js
@@ -0,0 +1,31 @@
+﻿const luffy = {
+  hasDevilFruitPowers: true,
+  powerLeverl: 100,
+};
+
+const zoro = {
+  hasDevilFruitPowers: false,
+  powerLeverl: 90,
+};
+
+const nami = {
+  hasDevilFruitPowers: false,
+  powerLeverl: 40,
+};
+
+const sanji = {
+  hasDevilFruitPowers: false,
+  powerLeverl: 85,
+};
+
+const chopper = {
+  hasDevilFruitPowers: true,
+  powerLeverl: 80,
+};
+
+const robin = {
+  hasDevilFruitPowers: true,
+  powerLeverl: 70,
+};
+
+module.exports = [luffy, zoro, sanji, nami, chopper, robin];
---
+++ index.js
@@ -0,0 +1,7 @@
+﻿const MUGIWARAS = require("./char.js");
+
+function sortByPowerLevel(arr) {}
+
+function sortByDevilFruit(arr) {}
+
+function addToTheTeam(newMember, arr) {}
