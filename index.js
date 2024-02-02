ed8cc3db32b8610f40192fd1f6d02546c4bdb5f2
Erbol Kasymbek <erbafredev@gmail.com>
2024-02-02 21:24:31 +0600

    sorted by power level

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 ﻿const MUGIWARAS = require("./char.js");
 
-function sortByPowerLevel(arr) {}
+function sortByPowerLevel(arr) {
+  return arr.toSorted((a, b) => a.powerLeverl - b.powerLeverl);
+}
+
+const sortedByPL = sortByPowerLevel(MUGIWARAS);
+console.log(sortedByPL);
 
 function sortByDevilFruit(arr) {}
 
