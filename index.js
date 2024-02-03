c5483fa44e9beec63f8ab4ba4a1cb515063504a3
aismai <abai.bakytbek@gmail.com>
2024-02-03 09:42:55 +0300

    revert changes

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 ﻿const MUGIWARAS = require("./char.js");
 
-function sortByPowerLevel(arr) {
-    return arr.sort(function (a, b) {
-        return a.powerLeverl - b.powerLeverl
-    })
-}
-console.log(sortByPowerLevel(MUGIWARAS));
+function sortByPowerLevel(arr) {}
 
-function sortByDevilFruit(arr) {
-    return arr.sort(function (a,b) {
-        return a.hasDevilFruitPowers - b.hasDevilFruitPowers
-    })
-    .map(function (elem) {
-        if(elem.hasDevilFruitPowers == true) {
-            console.log("Персонаж ел DevilFruit")
-        }
-        else {
-            console.log("Персонаж не ел DevilFruit")
-        }
-    })
-}
-sortByDevilFruit(MUGIWARAS);
+function sortByDevilFruit(arr) {}
 
-function addToTheTeam(newMember, arr) {
-
-    newMember = {hasDevilFruitPower: Boolean(), powerLeverl: Number(),}
-    arr.push(newMember)
-    return arr
-}
-let newTeamMate = addToTheTeam("black beard", MUGIWARAS)
-console.log(newTeamMate)
+function addToTheTeam(newMember, arr) {}
