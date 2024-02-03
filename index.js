8d0ba8e138060c878589545ffb5541066343e181
erkin <erkin.kasymbek@gmail.com>
2024-02-03 08:16:02 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 ﻿const MUGIWARAS = require("./char.js");
 
-function sortByPowerLevel(arr) {}
+function sortByPowerLevel(arr) {
+    return arr.sort(function (a, b) {
+        return a.powerLeverl - b.powerLeverl
+    })
+}
+console.log(sortByPowerLevel(MUGIWARAS));
 
-function sortByDevilFruit(arr) {}
+function sortByDevilFruit(arr) {
+    return arr.sort(function (a,b) {
+        return a.hasDevilFruitPowers - b.hasDevilFruitPowers
+    })
+    .map(function (elem) {
+        if(elem.hasDevilFruitPowers == true) {
+            console.log("Персонаж ел DevilFruit")
+        }
+        else {
+            console.log("Персонаж не ел DevilFruit")
+        }
+    })
+}
+sortByDevilFruit(MUGIWARAS);
 
-function addToTheTeam(newMember, arr) {}
+function addToTheTeam(newMember, arr) {
+
+    newMember = {hasDevilFruitPower: Boolean(), powerLeverl: Number(),}
+    arr.push(newMember)
+    return arr
+}
+let newTeamMate = addToTheTeam("black beard", MUGIWARAS)
+console.log(newTeamMate)
